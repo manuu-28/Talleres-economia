305cc98898ddf31ddaa74bb70d903103e20c1312
Mariana541-M <mariana.munozg@urosario.edu.co>
2026-08-30 20:35:19 -0500

    Create bar chart for internet usage by municipality
    Add visualization for internet usage by municipality.

--- scripts/tarea 1.R
+++ scripts/tarea 1.R
@@ -10,3 +10,21 @@ tenderos_tareaAgrupar <- tenderos_tarea %>%
   group_by(Munic_Dept) %>%
 summarise(internet = mean(uso_internet) * 100)
 View(tenderos_tareaAgrupar)
+
+# ---- Visualización ----
+# % de uso de internet por municipio/departamento
+grafico_tarea1 <- ggplot(tenderos_tareaAgrupar,
+                          aes(x = reorder(Munic_Dept, internet), y = internet)) +
+  geom_col(fill = "#2C7FB8") +
+  coord_flip() +
+  labs(
+    title = "Uso de internet por municipio/departamento",
+    x = "Municipio / Departamento",
+    y = "% de negocios que usan internet"
+  ) +
+  theme_minimal()
+
+grafico_tarea1
+
+ggsave("../graficos/tarea1_uso_internet_municipio.png",
+       plot = grafico_tarea1, width = 8, height = 6, dpi = 300)
