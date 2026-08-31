5fc516f81e8ce3da017f60669e864bb09b108308
Mariana541-M <mariana.munozg@urosario.edu.co>
2026-08-30 22:43:56 -0500

    Refactor data processing for tenderos analysis

--- scripts/tarea 3.R
+++ scripts/tarea 3.R
@@ -2,12 +2,12 @@ library(dplyr)
 library(rio)
 TenderosFU03_Publica <- import("../datos/TenderosFU03_Publica.dta")
 
-tenderos_tarea3=TenderosFU03_Publica %>%
-  select(uso_internet,actividad1,Munic_Dept)
-View(tenderos_tarea3)
-tenderos_tareaAgrupar2 <- tenderos_tarea3 %>%
-  group_by(Munic_Dept,actividad1) %>%
-summarise(internet=mean(uso_internet)*100)
+tenderos_tareaAgrupar2 <- TenderosFU03_Publica %>%
+  select(Munic_Dept, uso_internet, actG1:actG11) %>%
+  pivot_longer(cols = actG1:actG11, names_to = "actividad", values_to = "valor") %>%
+  filter(valor == 1) %>%
+  group_by(Munic_Dept, actividad) %>%
+  summarise(internet = mean(uso_internet, na.rm = TRUE) * 100, n_negocios = n(), .groups = "drop")
 View(tenderos_tareaAgrupar2)
 # ---- Visualización ----
 # % de uso de internet por municipio/departamento y tipo de actividad (heatmap)
